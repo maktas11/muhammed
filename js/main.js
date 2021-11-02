888fa810572501cfb6ca3ae29b7fa91c9b597546
maktas11 <degamegast11@gmail.com>
2021-11-02 23:46:35 +0100

    added projects

--- js/main.js
+++ js/main.js
@@ -21,5 +21,49 @@ if (buttonInfo) {
         text.innerHTML = "i do everything imaginable in this universe";
     })
 }
+
+const modal = document.querySelector(".myModal");
+const modal1 = document.querySelector(".myModal1");
+const modal2 = document.querySelector(".myModal2");
+const btn = document.querySelector(".myBtn");
+const btn1 = document.querySelector(".myBtn1");
+const btn2 = document.querySelector(".myBtn2");
+const span = document.getElementsByClassName("close")[0];
+const span1 = document.getElementsByClassName("close1")[0];
+const span2 = document.getElementsByClassName("close2")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+btn1.onclick = function() {
+  modal1.style.display = "block";
+}
+btn2.onclick = function() {
+  modal2.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+span1.onclick = function() {
+  modal1.style.display = "none";
+}
+span2.onclick = function() {
+  modal2.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+window.onclick = function(event) {
+  if (event.target == modal1) {
+    modal1.style.display = "none";
+  }
+}
+window.onclick = function(event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+}
   
   
